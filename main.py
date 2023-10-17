04a31108f921ef644fbecba86e37177f8e7db471
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-10-17 17:14:12 +0330

    init

--- main.py
+++ main.py
@@ -16,15 +16,14 @@ bucket_name = getenv('BUCKET_NAME')
 local_file_path = getenv('LOCAL_FILE')
 csv_data_path = getenv('CSV_DATA')
 
-# counter = 0
+counter = 0
 with open(csv_data_path, mode='r') as csv_file:
     csv_reader = reader(csv_file, delimiter=',')
     for row in csv_reader:
         try:
             client.fput_object(bucket_name, "public/" + row[1] + "/" + row[0].split("/")[-1], local_file_path + row[0])
             print(f'public/{row[1]}/{row[0].split("/")[-1]} <= {local_file_path}{row[0]}')
+            counter += 1
+            print(counter)
         except S3Error as e:
             print(f"Error uploading file: {e}")
-        # counter += 1
-        # if counter == 5:
-        #     break
