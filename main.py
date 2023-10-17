e5f6cc1ebf6e5ed7d2e845a8d3c39ed9ca9d361e
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-10-17 15:45:24 +0330

    init

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ bucket_name = getenv('BUCKET_NAME')
 local_file_path = getenv('LOCAL_FILE')
 csv_data_path = getenv('CSV_DATA')
 
-counter = 0
+# counter = 0
 with open(csv_data_path, mode='r') as csv_file:
     csv_reader = reader(csv_file, delimiter=',')
     for row in csv_reader:
         client.fput_object(bucket_name, "public/" + row[1] + row[0].split("/")[-1], local_file_path + row[0])
-        print(f'{local_file_path}{row[0]} => public/{row[1]}/{row[0].split("/")[-1]}')
-        counter += 1
-        if counter == 5:
-            break
+        print(f'public/{row[1]}/{row[0].split("/")[-1]} <= {local_file_path}/{row[0]}')
+        # counter += 1
+        # if counter == 5:
+        #     break
