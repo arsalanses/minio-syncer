00c56d7458104a49f583287371e49f3043f4d551
Arsalan Sefidgar <arsalan.sefidgar@gmail.com>
2023-10-18 10:21:44 +0330

    init

--- main.py
+++ main.py
@@ -16,14 +16,23 @@ bucket_name = getenv('BUCKET_NAME')
 local_file_path = getenv('LOCAL_FILE')
 csv_data_path = getenv('CSV_DATA')
 
+sizes = ['120x120', '150x150', '300x150', '300x169', '300x199', '300x200', '300x204', '300x230', '768x384', '768x432', '1024x512']
 counter = 0
 with open(csv_data_path, mode='r') as csv_file:
     csv_reader = reader(csv_file, delimiter=',')
     for row in csv_reader:
+        object_name = "public/" + row[1] + "/" + row[0].split("/")[-1]
+        file_path = local_file_path + row[0]
         try:
-            client.fput_object(bucket_name, "public/" + row[1] + "/" + row[0].split("/")[-1], local_file_path + row[0])
-            print(f'public/{row[1]}/{row[0].split("/")[-1]} <= {local_file_path}{row[0]}')
-            counter += 1
-            print(counter)
+            client.fput_object(bucket_name, object_name, file_path)
+            for size in sizes:
+                object_name_with_size = object_name[:-4] + '-' + size + object_name[-4:]
+                client.fput_object(bucket_name, object_name_with_size, file_path)
+                print(f'{counter} \t {object_name_with_size} <= {file_path}')
+                counter += 1
         except S3Error as e:
             print(f"Error uploading file: {e}")
+        # else:
+        #     print(f'success.')
+        # finally:
+        #     print(f'round completed.')
